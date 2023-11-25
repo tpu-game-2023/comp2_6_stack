d5c31372ffcb63357140c8da71f640d5f7f73d9e
imagire <imagire@gmail.com>
2023-11-25 23:44:28 +0900

    add files
    add files

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -10,48 +10,81 @@ void initialize(STACK* s, size_t mem_size)
 {
 	if (s == NULL) return;
 
-	// ToDo: mem_sizeでメモリを確保しよう
-	s->stack_pointer = NULL;
-	s->stack_memory = NULL;
-	s->end = NULL;
+	s->stack_memory = (int*)malloc(mem_size);
+	if (NULL == s->stack_memory) return;
+
+	s->end = s->stack_pointer = &s->stack_memory[mem_size/sizeof(int)];
 }
 
 
 // 確保したメモリを解放する
 void finalize(STACK* s)
 {
-	// ToDo: Initializeで確保したメモリを解放しよう
+	if (s == NULL) return;
+
+	if (s->stack_memory == NULL) return;
+
+	free(s->stack_memory);
+	s->stack_memory = NULL;
+	s->stack_pointer = NULL;
+	s->end = NULL;
 }
 
 
 // valの値をスタックに積む。実行の成否を返す
 bool push(STACK* s, int val)
 {
-	// ToDo: valの値をスタックに保存しよう
-	return false;
+	if (s == NULL) return false;
+
+	if (s->stack_pointer - 1 < s->stack_memory) {
+		return false;
+	}
+
+	*--s->stack_pointer = val;
+
+	return true;
 }
 
 
 // addrから始まるnum個の整数をスタックに積む。実行の成否を返す
 bool push_array(STACK* s, int* addr, int num)
 {
-	// ToDo: addrからはじまるnum個の整数をスタックに保存しよう
-	return false;
+	if (s == NULL) return false;
+	if (num <= 0) return false;
+
+	if (s->stack_pointer - num < s->stack_memory) {
+		return false;
+	}
+
+	s->stack_pointer -= num;
+	memcpy(s->stack_pointer, addr, num * sizeof(int));
+
+	return true;
 }
 
 // スタックから一つの要素を取り出す
 int pop(STACK* s)
 {
-	// ToDo: スタックの最上位の値を取り出して返そう
-	// 不具合時は0を返す
-	return 0;
+	if (s == NULL) return false;
+	
+	if (s->end < s->stack_pointer + 1) return 0;
+
+	return *s->stack_pointer++;
 }
 
 // addrにスタックからnumの要素を取り出す。取り出せた個数を返す
 int pop_array(STACK* s, int* addr, int num)
 {
-	// ToDo: スタックからnum個の値を取り出してaddrから始まるメモリに保存しよう
-	// スタックにnum個の要素がたまっていなかったら、積まれている要素を返して、
-	// 積んだ要素数を返り値として返そう
-	return 0;
+	if (s == NULL) return false;
+	if (addr == NULL) return false;
+
+	if (s->end < s->stack_pointer + num) {
+		num = s->end - s->stack_pointer;
+	}
+	if (num < 0) return 0;
+
+	memcpy(addr, s->stack_pointer, sizeof(int) * num);
+	s->stack_pointer += num;
+
+	return num;
 }
