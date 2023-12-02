db916014bdb53b9674960b858d2e7e0dd540dc92
mana0629 <ysangyuan885@gmail.com>
2023-12-02 22:54:25 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -10,48 +10,75 @@ void initialize(STACK* s, size_t mem_size)
 {
 	if (s == NULL) return;
 
-	// ToDo: mem_sizeでメモリを確保しよう
-	s->stack_pointer = NULL;
-	s->stack_memory = NULL;
-	s->end = NULL;
+	if (mem_size <= 0)
+	{
+		s->end = s->stack_memory = s->stack_pointer = NULL;
+		return;
+	}
+	s->stack_memory = (int*)malloc(mem_size);
+	s->end = s->stack_pointer = s->stack_memory + mem_size / sizeof(int);
 }
 
 
 // 確保したメモリを解放する
 void finalize(STACK* s)
 {
-	// ToDo: Initializeで確保したメモリを解放しよう
+	if (s == NULL) return;
+	free(s->stack_memory);
+	s->stack_memory = NULL;
 }
 
 
 // valの値をスタックに積む。実行の成否を返す
 bool push(STACK* s, int val)
 {
-	// ToDo: valの値をスタックに保存しよう
-	return false;
+	if (s == NULL || s->stack_pointer == s->stack_memory) return false;
+	
+	s->stack_pointer--;
+	*(s->stack_pointer) = val;
+	return true;
 }
 
 
 // addrから始まるnum個の整数をスタックに積む。実行の成否を返す
 bool push_array(STACK* s, int* addr, int num)
 {
-	// ToDo: addrからはじまるnum個の整数をスタックに保存しよう
-	return false;
+	if (s == NULL || addr == NULL || s->stack_memory > s->stack_pointer - num || num <= 0) return false;
+
+	for (int i = 0; i < num; i++)
+	{
+		push(s, addr[num - 1 - i]);
+	}
+	return true;
 }
 
 // スタックから一つの要素を取り出す
 int pop(STACK* s)
 {
-	// ToDo: スタックの最上位の値を取り出して返そう
-	// 不具合時は0を返す
-	return 0;
+	if (s == NULL || s->stack_pointer == s->end) return 0;
+
+	s->stack_pointer++;
+	return *(s->stack_pointer-1);
 }
 
 // addrにスタックからnumの要素を取り出す。取り出せた個数を返す
 int pop_array(STACK* s, int* addr, int num)
 {
-	// ToDo: スタックからnum個の値を取り出してaddrから始まるメモリに保存しよう
-	// スタックにnum個の要素がたまっていなかったら、積まれている要素を返して、
-	// 積んだ要素数を返り値として返そう
-	return 0;
+	if (s == NULL || addr == NULL || num <= 0) return 0;
+	
+	if (s->stack_pointer > s->end - num)
+	{
+		int cnum = s->end - s->stack_pointer;
+		for (int i=0; i < cnum; i++)
+		{
+			addr[i] = pop(s);
+		}
+		return cnum;
+	}
+
+	for (int i=0; i < num; i++)
+	{
+		addr[i] = pop(s);
+	}
+	return num;
 }
